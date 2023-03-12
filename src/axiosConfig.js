37f31a7ac18f04e536c5a50770152224acd5e439
selvaAU <selva.mariappan@anywhere.co>
2023-03-13 00:52:59 +0530

    Updated axiosConfig with localhost url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,6 +4,8 @@ const axiosInstance = axios.create({
     baseURL: 'http://35.154.22.94:8080/mybooksapp-0.0.1-SNAPSHOT/'
 });
 
+//    for localhost replace
+//    baseURL: 'http://localhost:8080/'
 
 
 // // Where you would set stuff like your 'Authorization' header, etc ...
