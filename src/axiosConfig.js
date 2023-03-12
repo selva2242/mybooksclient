6bf4e9a6e0c541482803f2c7ad54bfba412c2caf
selvaAU <selva.mariappan@anywhere.co>
2023-03-13 00:41:47 +0530

    Updated baseURL with live backend

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+    baseURL: 'http://35.154.22.94:8080/mybooksapp-0.0.1-SNAPSHOT/'
+//    for localhost
     baseURL: 'http://localhost:8080/'
+
 });
+
+
 
 // // Where you would set stuff like your 'Authorization' header, etc ...
 // instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
